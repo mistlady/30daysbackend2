b0db58813a360fbc55cf0a2ba51a4493c4d8ebdf
mistlady <127565435+mistlady@users.noreply.github.com>
2023-08-20 18:02:55 +0100

    Add files via upload

---
+++ coding-bootcamp-testimonials-slider-master/app2.js
@@ -0,0 +1,15 @@
+function changeNext() {
+  document.getElementById("tanya").src = "images/image-john.jpg";
+  document.getElementById("parag").innerHTML =
+    "“ If you want to lay the best foundation possible I’d recommend taking this course.The depth the instructors go into is incredible. I now feel so confident about starting up as a professional developer. ”";
+  document.getElementById("text").innerHTML = "John Tarkpor";
+  document.getElementById("subtext").innerHTML = "Junior Front-end Developer";
+}
+
+function changePrev() {
+  document.getElementById("tanya").src = "images/image-tanya.jpg";
+  document.getElementById("parag").innerHTML =
+    "“ I’ve been interested in coding for a while but never taken the jump, until now. I couldn’t recommend this course enough. I’m now in the job of my dreams and so excited about the future. ”";
+  document.getElementById("text").innerHTML = "Tanya Sinclair";
+  document.getElementById("subtext").innerHTML = "UX Engineer";
+}
